5b6b000918d2adfd7c455afc7d1823b274bb37cf
DarrelG <gautanadarrel06@gmail.com>
2025-10-10 21:32:01 +0700

    Make different route for each role

---
+++ resources/views/home/tutor.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+<link rel="stylesheet" href="{{ asset('css/homepage/style.css') }}">
+<script src="{{ asset('script/homepage/script.js') }}"></script>
+
+@section('content')
+<div class="homepage" style="max-width: 80rem; margin: 2rem auto;">
+    TEst Tutor
+</div>
+
+@endsection
